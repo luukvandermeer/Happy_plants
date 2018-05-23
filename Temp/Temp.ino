b17f059b146e16e96b9c47f70144318b7678f166
Luuk <luuk.vandermeer@gmail.com>
2018-05-23 20:56:25 +0200

    temp files

---
+++ Temp/Temp.ino
@@ -0,0 +1,32 @@
+#include <dht.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+dht DHT;
+
+#define DHT11_PIN 8
+
+void setup(){
+  lcd.begin(16, 2);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+    Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(1000);
+}
+
